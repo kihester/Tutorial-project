6087a53bda21578482154c25762d8365ed636568
Kieran Hester <kieran.hester@netcompany.com>
2022-03-02 09:59:06 +0000

    add test data

--- src/main/resources/db/migration/V1.2__test_data.sql
+++ src/main/resources/db/migration/V1.2__test_data.sql
@@ -1,7 +1,15 @@
-
-
 INSERT INTO book (name )
 VALUES ('The philosophers stone');
 
 INSERT INTO book (name)
 VALUES ('The chamber of secrets');
+
+INSERT INTO book (name )
+VALUES ('Chronicles of narnia');
+
+INSERT INTO book (name)
+VALUES ('Hobbit');
+
+INSERT INTO book (name )
+VALUES ('Game of thrones');
+
